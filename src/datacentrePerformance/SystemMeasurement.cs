b206f16751b1afa7cb114f0739e701b4faa6a5dc
PerezLamed <perez.lamed.vanniekerk@gmail.com>
2017-09-18 22:19:39 +0200

    small changes

--- src/datacentrePerformance/SystemMeasurement.cs
+++ src/datacentrePerformance/SystemMeasurement.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LamedalCore.domain.Attributes;
+using LamedalCore.domain.Enumerals;
 
 namespace datacentrePerformance
 {
+    [BlueprintRule_Class(enBlueprint_ClassNetworkType.VS_Static)]
     public static class SystemMeasurement
     {
+        
     }
 }
